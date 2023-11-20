d4888e9f1c95b5f8776a7e189ee74d21183d5f58
Bersen Naidoo <bersen.naidoo@gmail.com>
2023-11-20 15:32:09 +0200

    Implemented rootCmd

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,33 @@
 /*
-Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
+Copyright © 2023 Bersen Naidoo bersen.naidoo@gmail.com
 */
 package cmd
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
 
+	"github.com/bersennaidoo/devops/foundation/cert"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 )
 
+// Config is the internal representation of tls.yaml file.
+type Config struct {
+	CACert *cert.CACert          `yaml:"caCert"`
+	Cert   map[string]*cert.Cert `yaml:"certs"`
+}
 
+var cfgFilePath string
+var config Config
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "devops",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Short: "devops is a command line tool for TLS.",
+	Long: `devops is a command line tool for TLS.
+	Mainly used for generation of X.509 certificates, but can be extended`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -32,20 +35,31 @@ to quickly create a Cobra application.`,
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 }
 
+// init initializes cli application.
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+	cobra.OnInitialize(initConfig)
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.devops.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().StringVarP(&cfgFilePath, "config", "c", "", "config file (default is tls.yaml)")
 }
 
-
+// initConfig sets up and initializes CA config file.
+func initConfig() {
+	if cfgFilePath == "" {
+		cfgFilePath = "tls.yaml"
+	}
+	cfgFileBytes, err := ioutil.ReadFile(cfgFilePath)
+	if err != nil {
+		fmt.Printf("Error while reading config file: %s\n", err)
+		return
+	}
+	err = yaml.Unmarshal(cfgFileBytes, &config)
+	if err != nil {
+		fmt.Printf("Error while parsing config file: %s\n", err)
+		return
+	}
+}
